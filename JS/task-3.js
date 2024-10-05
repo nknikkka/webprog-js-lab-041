6fc8dcb0ecbb6dc3c44ed1b28b69f9461bb89993
nknikkka <kulikveronika14@gmail.com>
2024-10-05 20:23:38 +0300

    Onowleny2

--- JS/task-3.js
+++ JS/task-3.js
@@ -1,5 +1,5 @@
 function generatePassword(length = 8) {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const chars = 'ЙЦУКЕНГШЩЗХЇЄЖДЛОРПАВІФЯЧСМИТЬБЮйцукенгшщзхїфівапролджєячсмитьбюABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let password = '';
     
     for (let i = 0; i < length; i++) {
